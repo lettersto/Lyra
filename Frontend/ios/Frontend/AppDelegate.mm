5db69ec7dfa921e3681d4929dbfbda5e6f4ab972
윤주혜 <yunju421@hanmail.net>
2022-10-25 16:02:02 +0900

    google map API 셋팅

--- Frontend/ios/Frontend/AppDelegate.mm
+++ Frontend/ios/Frontend/AppDelegate.mm
@@ -16,6 +16,8 @@
 
 #import <react/config/ReactNativeConfig.h>
 
+#import <GoogleMaps/GoogleMaps.h>
+
 @interface AppDelegate () <RCTCxxBridgeDelegate, RCTTurboModuleManagerDelegate> {
   RCTTurboModuleManager *_turboModuleManager;
   RCTSurfacePresenterBridgeAdapter *_bridgeAdapter;
@@ -29,6 +31,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"[AIzaSyA23wCc-9bIbIYkyWGJ3m9rnsZvecORXS0]"];
+
   RCTAppSetupPrepareApp(application);
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
