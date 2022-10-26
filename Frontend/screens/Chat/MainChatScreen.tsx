3bfcac60a997b5abbd97404b979d8af0c959dacb
jh <jinju0405@naver.com>
2022-10-26 11:08:01 +0900

    [FEAT][F-09] 랜덤 이벤트 스크린 생성

--- Frontend/screens/Chat/MainChatScreen.tsx
+++ Frontend/screens/Chat/MainChatScreen.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import {Text} from 'react-native';
+import EventModal from './EventModal';
 
 const MainChatScreen = () => {
-  return <Text>MainChatScreen</Text>;
+  return (
+    <>
+      <Text>MainChatScreen</Text>
+      {/* LinearGradient 확인용 */}
+      <EventModal />
+    </>
+  );
 };
 
 export default MainChatScreen;
