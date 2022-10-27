5a942262e98a36b0374a80dc11a74b8baa9c5082
윤주혜 <yunju421@hanmail.net>
2022-10-27 14:01:12 +0900

    [FEAT] 버튼 컴포넌트 커스터마이징

--- Frontend/components/Utils/Button.tsx
+++ Frontend/components/Utils/Button.tsx
@@ -1,28 +1,120 @@
 import React from 'react';
-import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+import {
+  GestureResponderEvent,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import Colors from '../../constants/Colors';
 
-const Button = () => {
+type sizeType = 'extraSmall' | 'small' | 'medium' | 'large' | 'extraLarge';
+
+const Button = ({
+  title,
+  btnSize,
+  textSize,
+  customStyle,
+  isGradient,
+  isOutlined,
+  onPress,
+}: {
+  title: string;
+  btnSize: sizeType;
+  textSize: sizeType;
+  customStyle?: any;
+  isGradient: boolean;
+  isOutlined: boolean;
+  onPress: (event: GestureResponderEvent) => void;
+}) => {
+  const btnHeightSizes = {
+    extraSmall: 24,
+    small: 28,
+    medium: 32,
+    large: 36,
+    extraLarge: 40,
+  };
+
+  const btnTextSizes = {
+    extraSmall: 10,
+    small: 12,
+    medium: 14,
+    large: 16,
+    extraLarge: 18,
+  };
+
+  const btnHeightSize = btnHeightSizes[btnSize];
+  const btnTextSize = btnTextSizes[textSize];
+
+  const btnStyle = {
+    height: btnHeightSize,
+  };
+
+  const btnTextStyle = {
+    fontSize: btnTextSize,
+  };
+
+  const outlined = {
+    backgroundColor: Colors.black500,
+    height: '95%',
+    margin: 1,
+  };
+
+  const normal = {
+    backgroundColor: Colors.purple300,
+  };
+
+  if (isGradient) {
+    return (
+      <LinearGradient
+        start={{x: 0, y: 0}}
+        end={{x: 0, y: 1}}
+        useAngle={true}
+        angle={135}
+        angleCenter={{x: 0.5, y: 0.5}}
+        colors={[Colors.purple300, Colors.pink500]}
+        style={[styles.linearGradient, btnStyle]}>
+        {isOutlined ? (
+          <TouchableOpacity
+            style={[styles.buttonContainer, btnStyle, outlined, customStyle]}
+            onPress={onPress}>
+            <Text style={[styles.text, btnTextStyle]}>{title}</Text>
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity
+            style={[styles.buttonContainer, btnStyle, customStyle]}
+            onPress={onPress}>
+            <Text style={[styles.text, btnTextStyle]}>{title}</Text>
+          </TouchableOpacity>
+        )}
+      </LinearGradient>
+    );
+  }
   return (
-    <TouchableOpacity style={styles.button} onPress={() => console.log()}>
-      <Text style={styles.text}>확인</Text>
+    <TouchableOpacity
+      style={[styles.buttonContainer, normal, btnStyle, customStyle]}
+      onPress={onPress}>
+      <Text style={[styles.text, btnTextStyle]}>{title}</Text>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
-  button: {
-    backgroundColor: '#9194C6',
-    alignItems: 'center',
+  linearGradient: {
     justifyContent: 'center',
-    width: 94,
-    height: 44,
-    marginBottom: 30,
+    alignSelf: 'center',
+    borderRadius: 20,
+  },
+  buttonContainer: {
+    alignSelf: 'center',
+    justifyContent: 'center',
+    margin: 1,
     borderRadius: 20,
   },
   text: {
-    fontSize: 16,
     textAlign: 'center',
     color: 'white',
+    marginHorizontal: 10,
   },
 });
 
