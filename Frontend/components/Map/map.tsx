b60910cacb437c2200a7c63f42d53b6399e95503
윤주혜 <yunju421@hanmail.net>
2022-10-27 17:52:37 +0900

    [FEAT][D-01] 현재 위치 가져오기

--- Frontend/components/Map/map.tsx
+++ Frontend/components/Map/map.tsx
@@ -1,9 +1,47 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 import mapStyle from './mapStyle';
+import Geolocation from 'react-native-geolocation-service';
+import {View, Platform, PermissionsAndroid} from 'react-native';
+
+async function requestPermission() {
+  try {
+    // if (Platform.OS === "ios") {
+    //   return await Geolocation.requestAuthorization("always");
+    // }
+    // 안드로이드 위치 정보 수집 권한 요청
+    if (Platform.OS === 'android') {
+      return await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      );
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
 
 const Map = () => {
+  const [location, setLocation] = useState();
+  useEffect(() => {
+    requestPermission().then(result => {
+      console.log({result});
+      if (result === 'granted') {
+        Geolocation.getCurrentPosition(
+          pos => {
+            setLocation(pos.coords);
+          },
+          error => {
+            console.log(error);
+          },
+          {
+            enableHighAccuracy: true,
+            timeout: 3600,
+            maximumAge: 3600,
+          },
+        );
+      }
+    });
+  }, []);
   return (
     <>
       <View style={{flex: 1}}>
@@ -11,10 +49,10 @@ const Map = () => {
           style={{flex: 1}}
           provider={PROVIDER_GOOGLE}
           initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
+            latitude: location.latitude,
+            longitude: location.longitude,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005,
           }}
           customMapStyle={mapStyle}
         />
