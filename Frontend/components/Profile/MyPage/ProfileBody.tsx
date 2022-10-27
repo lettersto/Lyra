cf198c9c540306152e8dc6d54c7f178245b7f478
정혜령 <jhr3681@naver.com>
2022-10-27 09:31:12 +0900

    [FEAT][E-01] Add MainProfile and utility components

---
+++ Frontend/components/Profile/MyPage/ProfileBody.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+import ProfileItem from './ProfileItem';
+import CircleProfile from '../../Utils/CircleProfile';
+
+import MoreInfo from '../../Utils/MoreInfo';
+
+const ProfileBody = () => {
+  const dummyIntroduction = `안녕하세요!
+버스커 주혜입니다.\n
+어릴 때 영화 <어거스트 러쉬>를 보고 기타에 깊은 감명을 받아 버스킹을 시작하게 되었습니다.\n
+여러분에게도 따뜻한 감성을 전달하는 버스커가 되길 희망합니다.
+\n
+저는 주로 홍대 00로 00가게 앞 저녁 8시에 볼 수 있습니다.
+\n  
+많은 팔로우 부탁드려요!`;
+
+  return (
+    <View style={styles.profileContainer}>
+      <View style={styles.profileTop}>
+        <CircleProfile size="extraLarge" isGradient={false} />
+        {/* <Image source={require('../../../assets/image/basicProfile.png')} /> */}
+        <ProfileItem count={1} description="내 버스킹" />
+        <ProfileItem count={256} description="팔로워" />
+        <ProfileItem count={14} description="팔로우" />
+      </View>
+      <View>
+        <MoreInfo content={dummyIntroduction} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  profileContainer: {
+    width: '100%',
+    paddingHorizontal: 8,
+  },
+  profileTop: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingVertical: 16,
+  },
+});
+
+export default ProfileBody;
--- Frontend/screens/Profile/MainProfileScreen.tsx
+++ Frontend/screens/Profile/MainProfileScreen.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+
+import ProfileBody from '../../components/Profile/MyPage/ProfileBody';
+import WalletBody from '../../components/Profile/MyPage/WalletBody';
+import Gallery from '../../components/Profile/MyPage/Gallery';
+import GradientLine from '../../components/Utils/GradientLine';
+
+import Colors from '../../constants/Colors';
 
 const MainProfileScreen = () => {
-  return <Text>MainProfileScreen</Text>;
+  return (
+    <View style={styles.container}>
+      <ProfileBody />
+      <GradientLine />
+      <WalletBody />
+      <GradientLine />
+      <Gallery />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.black500,
+  },
+});
 
 export default MainProfileScreen;
