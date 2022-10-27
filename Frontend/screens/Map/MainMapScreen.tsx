fec2fa38bd3dbe3c02ba84ae26b37ff0652acad6
윤주혜 <yunju421@hanmail.net>
2022-10-27 15:36:17 +0900

    [FEAT][D-01] 지도 나이트 모드 스타일, 컴포넌트화

--- Frontend/screens/Map/MainMapScreen.tsx
+++ Frontend/screens/Map/MainMapScreen.tsx
@@ -1,22 +1,10 @@
 import React from 'react';
-import {View} from 'react-native';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import Map from '../../components/Map/map';
 
 const MainMapScreen = () => {
   return (
     <>
-      <View style={{flex: 1}}>
-        <MapView
-          style={{flex: 1}}
-          provider={PROVIDER_GOOGLE}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        />
-      </View>
+      <Map />
     </>
   );
 };
