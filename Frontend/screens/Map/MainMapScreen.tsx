6c412e5f0a1065f4e280420f78c3b37e1d19cd06
윤주혜 <yunju421@hanmail.net>
2022-10-28 17:16:54 +0900

    [STYLE][D-02] 카테고리 스타일 추가

--- Frontend/screens/Map/MainMapScreen.tsx
+++ Frontend/screens/Map/MainMapScreen.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import {Dimensions, StyleSheet} from 'react-native';
 import Map from '../../components/Map/map';
 import PheedCategory from '../../components/Pheed/Category/PheedCategory';
+
+const deviceWidth = Dimensions.get('window').width;
+const deviceHeight = Dimensions.get('window').height;
 
 const MainMapScreen = () => {
   return (
@@ -15,6 +18,10 @@ const MainMapScreen = () => {
 const styles = StyleSheet.create({
   category: {
     position: 'absolute',
+    zIndex: 1,
+    width: deviceWidth,
+    top: deviceHeight * 0.72,
+    backgroundColor: 'rgba(45, 45, 45, 0.9)',
   },
 });
 
