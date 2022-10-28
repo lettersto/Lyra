b3dc499389fcbacdc1abc7d824272b1089296df3
jh <jinju0405@naver.com>
2022-10-28 13:40:53 +0900

    [FEAT][C-01] 태그 카테고리 구현

--- Frontend/screens/Home/Pheed/CreatePheed.tsx
+++ Frontend/screens/Home/Pheed/CreatePheed.tsx
@@ -1,50 +1,68 @@
-import React, {useState} from 'react';
-import {SafeAreaView, Text, View, StyleSheet, Dimensions} from 'react-native';
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+  StyleSheet,
+  Dimensions,
+  KeyboardAvoidingView,
+  ScrollView,
+  TextInput,
+} from 'react-native';
 import Input from '../../../components/Utils/Input';
 import DateTime from '../../../components/Pheed/DateTime';
 import Colors from '../../../constants/Colors';
 import Gallery from '../../../components/Pheed/Gallery';
 import PheedCategory from '../../../components/Pheed/Category/PheedCategory';
+import Tag from '../../../components/Pheed/Tag';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
 const CreatePheed = () => {
   return (
     <>
-      <SafeAreaView>
-        <View style={styles.container}>
-          <Text style={styles.text}>Create Pheed</Text>
-          <Gallery />
-          <PheedCategory />
-          <View style={styles.inputCt}>
-            <Input
-              height={0.05}
-              width={0.9}
-              keyboard={1}
-              borderRadius={10}
-              placeholder="제목"
-              maxLength={30}
-            />
-            <Input
-              height={0.2}
-              width={0.9}
-              keyboard={1}
-              borderRadius={10}
-              placeholder="내용"
-              customStyle={styles.inputMargin}
-            />
+      <KeyboardAwareScrollView>
+        <SafeAreaView>
+          <View style={styles.container}>
+            <Text style={styles.text}>Create Pheed</Text>
+            <Gallery />
+            <PheedCategory />
+            <View style={styles.inputCt}>
+              <Input
+                height={0.05}
+                width={0.9}
+                keyboard={1}
+                borderRadius={10}
+                placeholder="제목"
+                maxLength={30}
+              />
+              <Input
+                height={0.2}
+                width={0.9}
+                keyboard={1}
+                borderRadius={10}
+                placeholder="내용"
+                customStyle={styles.inputMargin}
+              />
+            </View>
+            <View style={styles.dateplace}>
+              <DateTime />
+              <DateTime />
+            </View>
+            <Tag />
           </View>
-          <View style={styles.dateplace}>
-            <DateTime />
-            <DateTime />
-          </View>
-        </View>
-      </SafeAreaView>
+        </SafeAreaView>
+      </KeyboardAwareScrollView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
+  rootCt: {
+    flex: 1,
+  },
   container: {
     backgroundColor: Colors.black500,
+    // height: 500,
     height: Dimensions.get('window').height,
     paddingTop: 10,
   },
