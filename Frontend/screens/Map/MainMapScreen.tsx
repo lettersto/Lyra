890a9955994648826eb27340080b93518aaf7683
윤주혜 <yunju421@hanmail.net>
2022-10-28 16:02:05 +0900

    [FEAT][D-02] 지도 카테고리 버튼 추가

--- Frontend/screens/Map/MainMapScreen.tsx
+++ Frontend/screens/Map/MainMapScreen.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import {StyleSheet} from 'react-native';
 import Map from '../../components/Map/map';
+import PheedCategory from '../../components/Pheed/Category/PheedCategory';
 
 const MainMapScreen = () => {
   return (
     <>
       <Map />
+      <PheedCategory CustomStyle={styles.category} />
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  category: {
+    position: 'absolute',
+  },
+});
 
 export default MainMapScreen;
