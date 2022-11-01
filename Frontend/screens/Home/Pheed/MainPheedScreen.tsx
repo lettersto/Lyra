ee059fae0222298e3ac9beddebb0febcf51a5545
jh <jinju0405@naver.com>
2022-11-01 11:49:44 +0900

    [FEAT][G-05] 배너 구현

--- Frontend/screens/Home/Pheed/MainPheedScreen.tsx
+++ Frontend/screens/Home/Pheed/MainPheedScreen.tsx
@@ -1,29 +1,25 @@
 import React from 'react';
-import {
-  StyleSheet,
-  ImageBackground,
-  View,
-  ScrollView,
-  Dimensions,
-} from 'react-native';
+import {StyleSheet, View, ScrollView, Dimensions} from 'react-native';
 import CreateButton from '../../../components/Pheed/CreateButton';
 import PheedContent from '../../../components/Pheed/PheedContent';
+import Colors from '../../../constants/Colors';
+import MainBanner from '../../../components/Pheed/MainBanner';
 
 const MainPheedScreen = () => {
   return (
-    <ImageBackground
-      resizeMode="cover"
-      source={require('../../../assets/image/chatBackGroundImg.png')}
-      style={styles.container}>
-      <ScrollView style={styles.scroll}>
+    <ScrollView style={styles.scroll}>
+      <View style={styles.mainContainer}>
+        <View style={styles.bannerContainer}>
+          <MainBanner />
+        </View>
         <View style={styles.pheedContent}>
           <PheedContent />
         </View>
         <View style={styles.createBtn}>
           <CreateButton />
         </View>
-      </ScrollView>
-    </ImageBackground>
+      </View>
+    </ScrollView>
   );
 };
 
@@ -32,13 +28,19 @@ const styles = StyleSheet.create({
     flex: 1,
     position: 'relative',
   },
+  mainContainer: {
+    height: Dimensions.get('window').height * 0.83,
+  },
+  bannerContainer: {
+    height: 80,
+  },
   pheedContent: {
     flex: 1,
     alignSelf: 'center',
-    height: Dimensions.get('window').height * 0.83,
   },
   scroll: {
     flex: 1,
+    backgroundColor: Colors.black500,
   },
   createBtn: {},
 });
