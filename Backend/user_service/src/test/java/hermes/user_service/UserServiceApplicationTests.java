7ab64ce0eab8db2ff1c5efc5d9b7ce0096c83ab9
조영훈 <gcloud1025@gmail.com>
2022-11-02 15:20:45 +0900

    [FEAT] delete swagger

--- Backend/user_service/src/test/java/hermes/user_service/UserServiceApplicationTests.java
+++ Backend/user_service/src/test/java/hermes/user_service/UserServiceApplicationTests.java
@@ -2,7 +2,9 @@ package hermes.user_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootTest
 class UserServiceApplicationTests {
 
