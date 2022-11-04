f8a789751e003401845a4a6ef8b8508189f433b8
윤주혜 <yunju421@hanmail.net>
2022-11-04 13:25:22 +0900

    [FEAT][D-01] 지도 위치상 프로필 표시

--- Frontend/components/Map/map.tsx
+++ Frontend/components/Map/map.tsx
@@ -1,8 +1,11 @@
 import React, {useEffect, useState} from 'react';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 import Geolocation from 'react-native-geolocation-service';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 import MapStyle from './mapStyle';
+import Colors from '../../constants/Colors';
+import {profileData} from './profileData';
+import CircleProfile from '../Utils/CircleProfile';
 
 interface ILocation {
   latitude: number;
@@ -39,7 +42,7 @@ const Map = () => {
     <>
       <View style={{flex: 1}}>
         <MapView
-          style={{flex: 1, zIndex: -1}}
+          style={{flex: 1}}
           provider={PROVIDER_GOOGLE}
           initialRegion={{
             latitude: location.latitude,
@@ -49,11 +52,39 @@ const Map = () => {
           }}
           customMapStyle={MapStyle}
           showsUserLocation={true}
-          showsMyLocationButton={true}
-        />
+          showsMyLocationButton={true}>
+          {profileData.map((val, i) => {
+            // return <Marker coordinate={val.coords} image={val.img} />;
+            return (
+              <View key={i} style={[styles.profile]}>
+                <Marker coordinate={val.coords}>
+                  <CircleProfile grade="hot" size="medium" isGradient={true} />
+                </Marker>
+              </View>
+            );
+          })}
+        </MapView>
+        {/* <Marker coordinate={location}>
+          <Icon name="map-marker" size={20} color="white" />
+        </Marker> */}
       </View>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  profile: {
+    // position: 'absolute',
+    // zIndex: 1,
+    borderWidth: 3,
+    borderRadius: 100,
+    borderColor: Colors.purple300,
+  },
+  image: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+  },
+});
 
 export default Map;
---
+++ Frontend/components/Map/profileData.tsx
@@ -0,0 +1,52 @@
+export const profileData = [
+  {
+    id: 0,
+    coords: {
+      latitude: 37.5126,
+      longitude: 127.1025,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    },
+    img: require('../../assets/image/basicProfile.png'),
+  },
+  {
+    id: 1,
+    coords: {
+      latitude: 37.5089,
+      longitude: 127.0999,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    },
+    img: require('../../assets/image/basicProfile.png'),
+  },
+  {
+    id: 3,
+    coords: {
+      latitude: 37.5144,
+      longitude: 127.1007,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    },
+    img: require('../../assets/image/basicProfile.png'),
+  },
+  {
+    id: 4,
+    coords: {
+      latitude: 30.8406,
+      longitude: 76.9584,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    },
+    img: require('../../assets/image/basicProfile.png'),
+  },
+  {
+    id: 5,
+    coords: {
+      latitude: 30.6936,
+      longitude: 76.845,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    },
+    img: require('../../assets/image/basicProfile.png'),
+  },
+];
