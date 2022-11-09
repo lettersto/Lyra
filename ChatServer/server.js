d51f4f849825b3dd66392bbd1f3d0c56bb6db8f0
YunH <yunh04131@gmail.com>
2022-11-09 14:33:52 +0900

    [FEAT] 채팅 서버 유저 전부 내보내기 설정 및 유저 목록 설정

--- ChatServer/server.js
+++ ChatServer/server.js
@@ -11,49 +11,81 @@ const io = new Server(server, {
   },
 });
 
-let userDict = {}; // 유저가 들어간 방
+let userDict = {}; // 유저가 들어간 방정보
+let buskerDict = {};  // 현재 방에 있는 유저정보
 let buskerTotalDict = {}; // 누적으로 들어온 인원
-let buskerMaxCnt = {}; // 최대 동접자 수
-let buskerLiveDict = {}; // 진행중인 버스킹 방
 
-// 방에 있는 유저 수
-const fetchRoomCnt = async (io, buskerId) => {
-  const sockets = await io.in(buskerId).fetchSockets();
-  console.log(sockets);
-  // io.to(buskerId).emit("fetch user", buskerDict[buskerId].size);
+// 방에 있는 접속자 수
+const fetchRoomCnt = async (buskerId) => {
+  let cnt = 0
+  if (Object.keys(buskerDict).includes(String(buskerId))) {
+    cnt = buskerDict[buskerId].size
+  }
+  io.to(buskerId).emit("fetch user", cnt);
 };
 
 // 유저 입장
-const userIn = (userId, buskerId) => {
-  // 유저가 들어간 방 add
+const userIn = (socket, buskerId) => {
+  const userId = socket.data.userId;
+  // 유저의 방목록에 추가
   if (Object.keys(userDict).includes(String(userId))) {
     userDict[userId].add(buskerId);
   } else {
     userDict[userId] = new Set([buskerId]);
   }
-  // 버스커 방에 현재 들어온 인원 add
-  // if (Object.keys(buskerDict).includes(String(buskerId))) {
-  //   buskerDict[buskerId].add(userId);
-  // } else {
-  //   buskerDict[buskerId] = new Set([userId]);
-  // }
-  // 버스커 방 누적 인원 add
+  // 버스커 방에 유저 추가
+  if (Object.keys(buskerDict).includes(String(buskerId))) {
+    buskerDict[buskerId].add(userId);
+  } else {
+    buskerDict[buskerId] = new Set([userId]);
+  }
+  // 버스커 방 누적 인원 추가
   if (Object.keys(buskerTotalDict).includes(String(buskerId))) {
     buskerTotalDict[buskerId] = 1;
   } else {
-    buskerTotalDict[buskerId] = new Set([userId]);
-    // 유저 정보 보내주기
-    buskerLiveDict[buskerId] = setInterval(() => {
-      fetchRoomCnt(io, buskerId);
-    }, 10000);
+    buskerTotalDict[buskerId] = {};
   }
+  fetchRoomCnt(io, buskerId);
 };
 
 // 유저가 접속한 방 알려주기
-const fetchUserRooms = (userId) => {};
+const fetchUserRooms = (socket) => {
+  const userId = socket.data.userId;
+  const buskerIds = [];
+  if (Object.keys(userDict).includes(String(userId))) {
+    buskerIds = Array.from(userDict[userId]);
+  }
+  // 유저가 접속한 방들 보내기
+  socket.emit("user rooms", buskerIds);
+};
 
 // 유저 나가기
-const userOut = (userId, buskerId) => {};
+const userOut = (socket, buskerId) => {
+  const userId = socket.data.userId;
+  // 유저의 방목록에서 제거
+  if (Object.keys(userDict).includes(String(userId))) {
+    userDict[userId].delete(buskerId);
+  }
+  // 버스커 방에서 유저 제거
+  if (Object.keys(buskerDict).includes(String(buskerId))) {
+    buskerDict[buskerId].delete(userId);
+  }
+
+  fetchRoomCnt(io, buskerId);
+};
+
+// 유저 모두 내보내기
+const userAllOut = (buskerId) => {
+  const sockets = await io.in(buskerId).fetchSockets();
+  for (socket of sockets) {
+    const userId = socket.data.userId
+    if (Object.keys(userDict).includes(String(userId))) {
+      userDict[userId].delete(buskerId);
+    }
+  }
+  delete buskerDict[buskerId];
+  delete buskerTotalDict[buskerId];
+};
 
 // 해당 유저 연결
 io.on("connection", (socket) => {
@@ -67,13 +99,13 @@ io.on("connection", (socket) => {
   // 버스커 방에 연결한다.
   socket.on("enter room", async (buskerId) => {
     await socket.join(buskerId);
-    userIn(socket.data.userId, buskerId);
+    userIn(socket, buskerId);
     fetchRoomCnt(io, buskerId);
   });
 
   // 유저가 들어간 방들 조회
   socket.on("user rooms", (socket) => {
-    fetchUserRooms(io, socket.data.userId);
+    fetchUserRooms(io, socket);
   });
 
   // 전송된 메세지 받기
@@ -94,21 +126,23 @@ io.on("connection", (socket) => {
   // 버스커 방에서 나간다.
   socket.on("leave room", async (buskerId) => {
     await socket.leave(buskerId);
-    userOut(socket.data.userId, buskerId);
+    userOut(socket, buskerId);
+  });
+
+  // 버스커 방에서 모두 내보낸다.
+  socket.on("room close", async (buskerId) => {
+    if (buskerId === socket.data.userId) {
+      io.to(buskerId).emit("end",  buskerTotalDict[buskerId].size)
+      io.in(buskerId).socketsLeave(buskerId);
+      userAllOut(buskerId);
+    } else {
+      console.log("방장이 아닙니다!")
+    }
   });
 
   // 연결 끊어졌을 시!
   socket.on("disconnect", function () {
     console.log("user disconnected: ", socket.id);
-  });
-
-  // 버스커 방에서 모두 내보낸다.
-  socket.on("room close", async (buskerId) => {
-    io.in(buskerId).socketsLeave(buskerId);
-    clearInterval(buskerLiveDict[buskerId]);
-    delete buskerLiveDict[buskerId];
-    socket.emit("total cnt", buskerTotalDict[buskerId].size);
-    delete buskerTotalDict[buskerId];
   });
 });
 
