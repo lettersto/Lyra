cc6f7cdec9b5a07c22bd61dddf91de6d8f7796e8
조영훈 <gcloud1025@gmail.com>
2022-11-10 11:11:40 +0900

    [FEAT] user info update

--- Backend/Lyra/src/main/java/hermes/Lyra/dto/UserDto.java
+++ Backend/Lyra/src/main/java/hermes/Lyra/dto/UserDto.java
@@ -4,15 +4,33 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @NoArgsConstructor
 @Getter
 public class UserDto {
+    private Long id;
+    private String account;
+    private String bank;
+    private String image_url;
+    private String introduction;
+    private String refresh_token;
     private String email;
     private String nickname;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
 
     @Builder
-    public UserDto(String email, String nickname) {
+    public UserDto(Long id, String account, String bank, String image_url, String introduction, String refresh_token,  String email, String nickname, BigDecimal latitude, BigDecimal longitude) {
+        this.id = id;
         this.email = email;
         this.nickname = nickname;
+        this.account = account;
+        this.bank = bank;
+        this.image_url = image_url;
+        this.introduction = introduction;
+        this.refresh_token = refresh_token;
+        this.latitude = latitude;
+        this.longitude = longitude;
     }
 }
