83438ff622f253bc6838e384630e09e9c681f53c
정혜령 <jhr3681@naver.com>
2022-11-10 20:55:30 +0900

    [FEAT] add splash screen

--- Frontend/android/app/src/main/java/com/frontend/MainActivity.java
+++ Frontend/android/app/src/main/java/com/frontend/MainActivity.java
@@ -4,6 +4,7 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -41,6 +42,7 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
+    SplashScreen.show(this);
+    super.onCreate(savedInstanceState);
   }
 }
