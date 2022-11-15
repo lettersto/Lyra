7af310a106df4fdc4cdd98c1257b1ad426d73ba4
윤주혜 <yunju421@hanmail.net>
2022-11-15 14:09:57 +0900

    [FIX] 동네 설정 stack, location Info 출력 수정

--- Frontend/store/map-context.tsx
+++ Frontend/store/map-context.tsx
@@ -9,15 +9,15 @@ import React, {
 export interface MapContextType {
   userLocationInfo: null | string;
   userRegionCode: null | string;
-  userLatitude: null | number;
-  userLongitude: null | number;
+  userLatitude: number;
+  userLongitude: number;
   mapLatitude: number;
   mapLongitude: number;
   mapZoomLv: number;
   setUserLocationInfo: Dispatch<SetStateAction<string | null>>;
   setUserRegionCode: Dispatch<SetStateAction<string | null>>;
-  setUserLatitude: Dispatch<SetStateAction<number | null>>;
-  setUserLongitude: Dispatch<SetStateAction<number | null>>;
+  setUserLatitude: Dispatch<SetStateAction<number>>;
+  setUserLongitude: Dispatch<SetStateAction<number>>;
   setMapLatitude: Dispatch<SetStateAction<number>>;
   setMapLongitude: Dispatch<SetStateAction<number>>;
   setMapZoomLv: Dispatch<SetStateAction<number>>;
@@ -26,8 +26,8 @@ export interface MapContextType {
 export const MapContext = createContext<MapContextType>({
   userLocationInfo: null,
   userRegionCode: null,
-  userLatitude: null,
-  userLongitude: null,
+  userLatitude: 0,
+  userLongitude: 0,
   mapLatitude: 0,
   mapLongitude: 0,
   mapZoomLv: 17,
@@ -43,8 +43,8 @@ export const MapContext = createContext<MapContextType>({
 export const MapProvider = ({children}: {children: ReactNode}) => {
   const [userLocationInfo, setUserLocationInfo] = useState<string | null>(null);
   const [userRegionCode, setUserRegionCode] = useState<string | null>(null);
-  const [userLatitude, setUserLatitude] = useState<number | null>(null);
-  const [userLongitude, setUserLongitude] = useState<number | null>(null);
+  const [userLatitude, setUserLatitude] = useState<number>(0);
+  const [userLongitude, setUserLongitude] = useState<number>(0);
   const [mapLatitude, setMapLatitude] = useState<number>(0);
   const [mapLongitude, setMapLongitude] = useState<number>(0);
   const [mapZoomLv, setMapZoomLv] = useState<number>(17);
