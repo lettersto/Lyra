ef3aae535808b06101daaa6c3ea6cf9e5570b2cf
yuzu <myuju419@naver.com>
2022-11-19 15:52:50 +0900

    [FIX] delete wallet

--- Backend/Lyra/src/main/java/hermes/Lyra/Service/WalletServiceImpl.java
+++ Backend/Lyra/src/main/java/hermes/Lyra/Service/WalletServiceImpl.java
@@ -11,6 +11,7 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
@@ -60,6 +61,7 @@ public class WalletServiceImpl implements WalletService {
     }
 
     @Override
+    @Transactional
     public void deleteWallet(Long userId) {
         Wallet w = walletRepository.getByUserId(userId);
 
